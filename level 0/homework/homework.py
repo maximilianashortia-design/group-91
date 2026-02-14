d4a491456e4d08f9ffa7f61db76e89f3bbc6ba43
maximilianashortia-design <maximilianashortia@gmail.com>
2026-02-14 19:44:09 +0400

    second commit

---
+++ level 0/homework/homework.py
@@ -0,0 +1,13 @@
+#რომ შევქმნათ პითონის ფაილი ჩვენ უნდა შევქმნათ ფაილი და დავუწეროთ გაფართოება .py ბოლოში
+#print() ფუნქცია გამოიყენება რათა ინფორმაცია ეკრანზე გადავიტანოთ
+#კომენატარი არის ტექსტი რომელსაც პროგრამა არ ასრულებს ის გამოიყენება რათა კოდი უფრო გასაგები გახდეს
+#terminal არის ვიზუალური სივრცე რომლითაც შეგიძლია პროგრამა გაუშვა და პროგრამის მოქმედებას უთვალყურო
+print("Max Ashortia")
+print("13")
+print("Breaking Bad")
+print("Dexter")
+print("Star Wars")
+print("The Rush Hour")
+print("Interstellar")
+print("Max Max Max Max Max Max Max Max Max Max Max Max")
+print("18")
