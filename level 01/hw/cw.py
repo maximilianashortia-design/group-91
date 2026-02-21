3864b2bb14ec495841e332b4bd6a026270fb2462
maximilianashortia-design <maximilianashortia@gmail.com>
2026-02-21 16:41:33 +0400

    second commit

---
+++ level 01/hw/cw.py
@@ -0,0 +1,14 @@
+#კომენტარი ეხმარება ადამიანს გაიგოს უფრო მარტივად თუ რას აკეთებს ქვემოთ დაწერილი კოდი 
+#ფაილის გაფართოება არის მისაჩვენებელი თუ რო კოდების ენაში უნდა დავწეროთ
+#print ვიყენებთ რათა ტერმინალში გადავიტანოთ ტექსტი
+#terminal არის ვიზუალური სივრცე სადაც ჩვენი კოდის შედეგი შეგვიძლია დავინახოთ
+print("Max")
+print("Ashortia")
+print(12)
+print("Breaking Bad")
+print("Lupin")
+print("Interstellar")
+print("Dexter")
+print("Star Wars")
+print("Max Max Max Max Max Max Max Max Max Max Max")
+print(18)
